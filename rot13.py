e8157cd6d8fef898c0bc46feba5325e3afb3e1ab
Haroon S <haroon@users.noreply.github.com>
2021-10-31 01:19:30 +1100

    Add files via upload

--- rot13.py
+++ rot13.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def rot13(str_in):
     str_out = ""
     for char_in in str_in:
@@ -9,3 +12,23 @@ def rot13(str_in):
             str_out += char_in
 
     return str_out
+
+
+class Tests(unittest.TestCase):
+    def test_alpha(self):
+        test_strs = ['this is a test string', 'asfsjfhjfsd fasdsaf oruewoufh', 'a', 'asf243rf4']
+        for test_str in test_strs:
+            str_cipher = rot13(test_str)
+            self.assertNotEqual(str_cipher, test_str)
+            str_plain = rot13(str_cipher)
+            self.assertEqual(str_plain, test_str)
+
+    def test_num(self):
+        test_str = '123457567'
+        str_cipher = rot13(test_str)
+        self.assertEqual(str_cipher, test_str)
+        str_plain = rot13(str_cipher)
+        self.assertEqual(str_plain, test_str)
+
+if __name__ == '__main__':
+    unittest.main()
