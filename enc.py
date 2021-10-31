53d95b481426abaa8f3e7f5c4750c1beeacb5b84
gitpushpull <32915474+gitpushpull@users.noreply.github.com>
2021-11-01 03:18:50 +1100

    Added unit test for base64 encode script

--- enc.py
+++ enc.py
@@ -1,4 +1,5 @@
 import base64
+import unittest
 
 
 def enc16(plain_text):
@@ -23,3 +24,24 @@ def enc64(plain_text):
 def dec64(enc_text):
     return base64.b64decode(enc_text).decode("utf-8")
 
+msgs = {
+	'hello world': [b'68656C6C6F20776F726C64', b'NBSWY3DPEB3W64TMMQ======', b'aGVsbG8gd29ybGQ='],
+	'HELLO WORLD': [b'48454C4C4F20574F524C44', b'JBCUYTCPEBLU6USMIQ======', b'SEVMTE8gV09STEQ=']
+}
+
+
+class Tests(unittest.TestCase):
+    def test_enc(self):
+        for msg in msgs:
+            self.assertEqual(msgs[msg][0], enc16(msg))
+            self.assertEqual(msgs[msg][1], enc32(msg))
+            self.assertEqual(msgs[msg][2], enc64(msg))
+
+    def test_dec(self):
+        for msg in msgs:
+            self.assertEqual(msg, dec16(msgs[msg][0]))
+            self.assertEqual(msg, dec32(msgs[msg][1]))
+            self.assertEqual(msg, dec64(msgs[msg][2]))
+
+if __name__ == '__main__':
+    unittest.main()
